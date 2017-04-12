206ddd183f0277024ffb8ac12f25e2c3a941f473
Prashant Bansal <prashantban@gmail.com>
2017-04-11 22:14:07 -0400

    Server Actions

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 // server/app.js
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+const express = require('express'),
+	morgan = require('morgan'),
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	routes = require('./routes/index'),
+	redis = require("redis");
 
 const app = express();
+const redisClient = redis.createClient('port', 'url'),
+	secret = "Secret Message";
 
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
@@ -13,13 +16,28 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 // Serve static assets
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
+// Serve Routes File
 app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Access request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+redisClient.on('connect', function() {
+  console.log('Connected to Redis');
 });
+
+// Setup JWTRedis Session
+app.use(JWTRedisSession({
+  client: redisClient,
+  secret: secret,
+  keyspace: "sess:",
+  maxAge: 86400,
+  algorithm: "HS256",
+  requestKey: "jwtSession",
+  requestArg: "jwtToken"
+}));
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,55 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
+const express = require('express'),
+	router = express.Router(),
+	path = require('path'),
+	JWTRedisSession = require("jwt-redis-session"),
+	request = require("request");
+
+
+/** Login Route
+ * Create session in redis
+ * @return token
+ */
+router.post('/user/login', (req, res) => {
+
+  const options = {
+    url: 'API Server URL',
+    body: req.body,
+  };
+
+  request.post(options, (error, response, body) => {
+    if (response.statusCode >= 200 && response.statusCode <=304) {
+      // this will be attached to the JWT
+      var claims = {
+        user: body.user,
+      };
+      // create session & return the token
+      req.jwtSession.create(claims, (error, token) => {
+        res.json({
+          access_granted: true,
+          token: token
+        });
+      });
+    } 
+
+    // Error Occured
+    else {
+      res.status(500).json({
+        access_granted: false
+      });
+    }
+  });
+});
+
+/** Logout Route
+ * Log Out User & Destroy Redis Session
+ */
+router.post('/user/logout', (req, res) => {
+  req.jwtSession.destroy(error => {
+    res.json({
+      logout: true
+    });
+  });
+});
 
 // Always return the main index.html, so react-router render the route in the client
 router.get('*', (req, res) => {
